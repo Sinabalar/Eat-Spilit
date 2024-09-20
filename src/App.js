2bd7b7246c790b42b64d5f7c0186f4291f99a05f
Sinabalar <sinabalar666@gmail.com>
2024-09-20 18:05:42 +0330

    the static components is fully created (there is no State)

--- src/App.js
+++ src/App.js
@@ -1,71 +1,114 @@
-import style from "./index.css";
+    import style from "./index.css";
 
-const initialFriends = [
-    {
-        id: 118836,
-        name: "Clark",
-        image: "https://i.pravatar.cc/48?u=118836",
-        balance: -7,
-    },
-    {
-        id: 933372,
-        name: "Sarah",
-        image: "https://i.pravatar.cc/48?u=933372",
-        balance: 20,
-    },
-    {
-        id: 499476,
-        name: "Anthony",
-        image: "https://i.pravatar.cc/48?u=499476",
-        balance: 0,
-    },
-];
+    const initialFriends = [
+        {
+            id: 118836,
+            name: "Clark",
+            image: "https://i.pravatar.cc/48?u=118836",
+            balance: -7,
+        },
+        {
+            id: 933372,
+            name: "Sarah",
+            image: "https://i.pravatar.cc/48?u=933372",
+            balance: 20,
+        },
+        {
+            id: 499476,
+            name: "Anthony",
+            image: "https://i.pravatar.cc/48?u=499476",
+            balance: 0,
+        },
+    ];
 
 
-export default function App() {
-    return (
-        <div className='app'>
-            <div className='sidebar'>
-                <FriendsList/>
+    export default function App() {
+        return (
+            <div className='app'>
+                <div className='sidebar'>
+                    <FriendsList/>
+                    <FormAddFriend/>
+                    <Button>Add Friend</Button>
+                </div>
+                <FormSpilitBill/>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
-function FriendsList() {
-    const friends = initialFriends;
-    return (
-        <ul>
-            {
-                friends.map(el => <Friend friendItem={el} key={el.id}/>)
-            }
-        </ul>
-    )
-}
+    function FriendsList() {
+        const friends = initialFriends;
+        return (
+            <ul>
+                {
+                    friends.map(el => <Friend friendItem={el} key={el.id}/>)
+                }
+            </ul>
+        )
+    }
 
-function Friend({friendItem}) {
-    return (
-        <li>
-            <img src={friendItem.image} alt={friendItem.name}/>
-            <h3>{friendItem.name}</h3>
-            {
-                (friendItem.balance < 0) &&
-                <p className='red'>You owe {friendItem.name} {Math.abs(friendItem.balance)}$</p>
+    function Friend({friendItem}) {
+        return (
+            <li>
+                <img src={friendItem.image} alt={friendItem.name}/>
+                <h3>{friendItem.name}</h3>
+                {
+                    (friendItem.balance < 0) &&
+                    <p className='red'>You owe {friendItem.name} {Math.abs(friendItem.balance)}$</p>
 
-            }
-            {
-                (friendItem.balance > 0) &&
-                <p className='green'>{friendItem.name} owes you {Math.abs(friendItem.balance)}$</p>
+                }
+                {
+                    (friendItem.balance > 0) &&
+                    <p className='green'>{friendItem.name} owes you {Math.abs(friendItem.balance)}$</p>
 
-            }
-            {
-                (friendItem.balance === 0) &&
-                <p>You and  {friendItem.name} are even</p>
+                }
+                {
+                    (friendItem.balance === 0) &&
+                    <p>You and  {friendItem.name} are even</p>
 
-            }
+                }
 
-            <button className='button'>Select</button>
+                <Button>Select</Button>
 
-        </li>
-    )
-}
+            </li>
+        )
+    }
+
+    function FormAddFriend(){
+        return(
+            <form className='form-add-friend'>
+                <label>Friend Name</label>
+                <input placeholder={'Name'} type={'text'}/>
+                <label>📷 Image URL</label>
+                <input placeholder={'URL'} type={'text'}/>
+                <Button>Add</Button>
+            </form>
+        )
+    }
+
+    function Button({children}){
+        return(
+            <button className={'button'}>{children}</button>
+        )
+    }
+
+    function FormSpilitBill(){
+        return(
+            <form className={'form-split-bill'}>
+                <h2>Split a bill with X</h2>
+                <label>💰 Bill value</label>
+                <input placeholder={'$'} type={'text'}/>
+                <label>Your expense</label>
+                <input type={'text'}/>
+                <label>X's expense</label>
+                <input type={'text'} disabled/>
+                <label>🤔 Who is paying the bill ?</label>
+                <select>
+                    <option value={'user'}>Me</option>
+                    <option value={'friend'}>X</option>
+                </select>
+                <Button>Spilit bill</Button>
+
+
+            </form>
+        )
+    }
